dd12f6cfe186cec1036d008dd2518702272801ef
isuapp <isaacmarti.g@gmail.com>
2022-05-03 18:36:26 +0200

    Resultados que coin ciden con varios pokemon

--- src/index.js
+++ src/index.js
@@ -3,20 +3,36 @@
 const input = document.getElementById('search');
 
 console.log(input.value);
-const pokemons = [];
-const search = async () => {
+
+const getAllPokemons = async () => {
   try {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon?limit=1126`
     );
     const data = await response.json();
-    pokemons.push(data.results);
+    console.log(data);
+    return await pokemons.push(...data.results);
   } catch (err) {
     console.error(err);
   }
 };
-search();
-console.log(pokemons[0]);
+
+getAllPokemons();
+
+let pokemons = [];
+
+const filteredPokemons = async (pokemons, query) => {
+  const awaitResults = await Promise.all(
+    pokemons.filter((pokemon) => {
+      return pokemon.name.indexOf(query.toLowerCase()) !== -1;
+    })
+  );
+  console.log(awaitResults);
+};
+console.log(setTimeout(filteredPokemons(pokemons, 'char'), 5000));
+
 input.addEventListener('keypress', (e) => {
-  e.key === 'Enter' ? search() : console.error('error');
+  e.key === 'Enter'
+    ? search()
+    : setTimeout(filteredPokemons(pokemons, input.value), 5000);
 });
