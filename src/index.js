7b09f0ed0a35f623bbe8dff8bd0761aa993f4c7a
isuapp <isaacmarti.g@gmail.com>
2022-05-03 11:20:46 +0200

    Función search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const input = document.getElementById('search');
+
+console.log(input.value);
+const search = async () => {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${input.value}`
+    );
+    const data = await response.json();
+    console.log('data', data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    search();
+  }
+});
