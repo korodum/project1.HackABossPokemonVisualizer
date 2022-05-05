594d04d37938b6dd0f7936f5e51ec43ffbf107aa
isuapp <isaacmarti.g@gmail.com>
2022-05-05 13:40:57 +0200

    Filtrado por tipo añadido

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 'use strict';
-import { getAllPokemonNames, loadPkmn, filteredPokemons, pokemonNames, pkmnList } from "./modules/Filter.module.js";
-import { getAllPokemons, pokemons} from './modules/ShowPokemon.module.js';
+import {
+  getAllPokemonNames,
+  loadPkmn,
+  filteredPokemons,
+  pokemonNames,
+  pkmnList,
+} from './modules/Filter.module.js';
+import { getAllPokemons, pokemons } from './modules/ShowPokemon.module.js';
+import {
+  getAllPokemonTypes,
+  loadPokemonTypes,
+  pokemonTypes,
+  typesList,
+  pkmType,
+} from './modules/FilterByType.module.js';
 
 const input = document.getElementById('search');
-
-
-getAllPokemonNames()
+getAllPokemonNames();
 getAllPokemons();
-input.addEventListener('input', function() {
+
+input.addEventListener('input', function () {
   const filteredPkmn = filteredPokemons(pokemonNames, input.value);
   loadPkmn(filteredPkmn, pkmnList);
-})
+});
+
+//
+//
+//Recogemos todos los typos de pokémon y los pintamos
+getAllPokemonTypes();
+
+//Click para seleccionar el tipo de pokémon elegido
+typesList.addEventListener('click', (e) => {
+  const { target } = e;
+  if (target.matches('li.type')) {
+    console.log('match motherfucker', target.innerText);
+    let pkmType = target.innerText;
+    console.log(pkmType);
+    console.log(`pokemons del tipo ${pkmType}`, getAllPokemonTypes());
+  } else {
+    console.error('Error on target pokemon type');
+  }
+});
+
+console.log(pkmType);
