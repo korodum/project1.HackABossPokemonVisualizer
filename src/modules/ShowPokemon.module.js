fb2b22913c671bd68fd6f2a019b5207820946d95
Karim <4bkarim.amaioua@gmail.com>
2022-05-06 09:16:53 +0100

    show pokemon almost finished

--- src/modules/ShowPokemon.module.js
+++ src/modules/ShowPokemon.module.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const name = document.getElementById('pokemon-name')
+const height = document.getElementById('height')
+const weight = document.getElementById('weight')
+const hp =document.getElementById('hp')
+const attack =document.getElementById('attack')
+const defense =document.getElementById('defense')
+const specialAttack =document.getElementById('special-attack')
+const specialDefense =document.getElementById('special-defense')
+const speed =document.getElementById('speed')
 
 async function getAllPokemons() {
   try {
@@ -33,9 +41,18 @@ async function getPokemonInfo (input) {
 async function showPokemon (input) {
   try {
     const pkmn = await getPokemonInfo(input);
-    const pkmnRes = await fetch(pkmn.url)
-    const pkmnData = await pkmnRes.json()
-    return name.innerText = pkmnData.species.name
+    const pkmnRes = await fetch(pkmn.url);
+    const pkmnData = await pkmnRes.json();
+    name.value = pkmnData.species.name;
+    height.value = `${pkmnData.height/10} meters`;
+    weight.value = `${pkmnData.weight/10} kilos`;
+    hp.value = pkmnData.stats[0].base_stat
+    attack.value = pkmnData.stats[1].base_stat
+    defense.value = pkmnData.stats[2].base_stat
+    specialAttack.value = pkmnData.stats[3].base_stat
+    specialDefense.value = pkmnData.stats[4].base_stat
+    speed.value = pkmnData.stats[5].base_stat
+
   } catch (error) {
     console.error(error);
   }
