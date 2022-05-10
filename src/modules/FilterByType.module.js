3d86c0105851e7ff582a42b880e9b560856bb986
isuapp <isaacmarti.g@gmail.com>
2022-05-10 18:16:54 +0200

    Problema al filtrar por tipos de pokémon (en el array filtrado, aparecen los cinos e impiden elegir pokémon

--- src/modules/FilterByType.module.js
+++ src/modules/FilterByType.module.js
@@ -1,45 +1,43 @@
 'use strict';
-
 const typesList = document.getElementById('types-list');
 let pokemonTypes = [];
-;
-
 async function getAllPokemonTypes() {
   try {
-
-  const res = await fetch(`https://pokeapi.co/api/v2/type`)
-  const data = await res.json()
-  pokemonTypes = data.results.map((type) => type.name);
-  pokemonTypes.sort();
-  loadPokemonTypes(pokemonTypes, typesList);
+    const res = await fetch(`https://pokeapi.co/api/v2/type`);
+    const data = await res.json();
+    pokemonTypes = data.results.map((type) => type.name);
+    pokemonTypes.sort();
+    loadPokemonTypes(pokemonTypes, typesList);
   } catch (error) {
-      console.error(error);
+    console.error(error);
   }
-
 }
 
 function loadPokemonTypes(data, element) {
   if (data) {
     element.innerHTML = '';
     let innerElement = '';
-    data.forEach((item) => {
+    for (let i = 0; i < data.length; i++) {
       innerElement += `
-        <li class="type">${item}</li>`;
-    });
+        <li class="type">
+        ${data[i]}
+        <img src="./src/assets/imgs/types/${
+          i + 1
+        }.svg"  style="width:2rem;height:2rem"/>
+        </li>`;
+    }
     element.innerHTML = innerElement;
   }
 }
 
-
 async function filteringByType(type) {
   const res = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
-  const data = await res.json()
-  let filteredPokemonsByType = []
-  filteredPokemonsByType = data.pokemon.map((pokemon) => pokemon.pokemon.name)
-  filteredPokemonsByType.sort()
+  const data = await res.json();
+  let filteredPokemonsByType = [];
+  filteredPokemonsByType = data.pokemon.map((pokemon) => pokemon.pokemon.name);
+  filteredPokemonsByType.sort();
 
   return filteredPokemonsByType;
-
 }
 
 export {
